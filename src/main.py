b605eff6e71d3923606d3c10f180aebacff055ba
Ian Carlos Lima Tavares <xxxiancarlosxxx@gmail.com>
2026-03-07 00:30:16 -0300

    Adicionando funcionalidades finais

--- src/main.py
+++ src/main.py
@@ -4,6 +4,16 @@ import models
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 models.Base.metadata.create_all(bind=engine)
 
 from auth_routes import auth_router
@@ -11,3 +21,18 @@ from weather_routes import weather_router
 
 app.include_router(auth_router)
 app.include_router(weather_router)
+
+from fastapi.responses import FileResponse
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+@app.get("/")
+async def read_login():
+    login_path = os.path.join(BASE_DIR, "frontend", "login.html")
+    return FileResponse(login_path)
+
+@app.get("/index.html")
+async def read_index():
+    index_path = os.path.join(BASE_DIR, "frontend", "index.html")
+    return FileResponse(index_path)
--- src/weather_routes.py
+++ src/weather_routes.py
@@ -48,3 +48,49 @@ async def sendCity(city_name: str, db: Session = Depends(database.get_db), user:
     db.refresh(new_history)
 
     return {"status": "sucesso", "id": new_history.id}
+
+@weather_router.put("/update_history/{history.id}")
+async def update_history(history_id: int, db: Session = Depends(database.get_db), user: models.User = Depends(security.get_current_user)):
+    db_history = db.query(models.SearchHistory).filter(models.SearchHistory.id == history_id, models.SearchHistory.user_id == user.id).first()
+
+    if not db_history:
+        raise HTTPException(status_code=404, detail="Registro não encontrado.")
+    
+    coords = await weather_services.get_coordinates(db_history.city_name)
+    weather_data = await weather_services.get_weather_forecast(lat=coords["lat"], lon=coords["lon"])
+
+    if "current" not in weather_data:
+        raise HTTPException(status_code=400, detail="Erro ao atualizar dados via API.")
+    
+    db_history.temp_current = weather_data["current"]["temp"]
+    db_history.temp_min = weather_data["current"]["temp_min"]
+    db_history.temp_max = weather_data["current"]["temp_max"]
+    db_history.feels_like = weather_data["current"]["feels_like"]
+    db_history.rain_prob = weather_data["current"]["pop"]
+    db_history.condition_desc = weather_data["current"]["description"]
+    db_history.timestamp = datetime.now()
+
+    db.commit()
+    db.refresh(db_history)
+
+    return {"message": "Dados atualizados com sucesso via API", "item": db_history}
+
+@weather_router.delete("/delete_history/{history_id}")
+async def delete_history(history_id: int, db: Session = Depends(database.get_db), user: models.User = Depends(security.get_current_user)):
+    db_history = db.query(models.SearchHistory).filter(models.SearchHistory.id == history_id, models.SearchHistory.user_id == user.id).first()
+
+    if not db_history:
+        raise HTTPException(status_code=404, detail="Registro não encontrado.")
+
+    db.delete(db_history)
+    db.commit()
+
+    return {"status": "removido", "id": history_id}
+
+@weather_router.get("/recent_history")
+async def get_recent_history(db: Session = Depends(database.get_db), user: models.User = Depends(security.get_current_user)):
+    query = db.query(models.SearchHistory).filter(models.SearchHistory.user_id == user.id)
+
+    recent_searches = query.order_by(models.SearchHistory.timestamp.desc()).limit(3).all()
+
+    return recent_searches
